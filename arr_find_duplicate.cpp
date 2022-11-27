fea34eb27588f4c204ad013bcd5b8e5753a59c4f
Tanya Nagar <114303703+TanyaNagar@users.noreply.github.com>
2022-11-27 12:39:38 +0530

    Add files via upload

---
+++ arr_find_duplicate.cpp
@@ -0,0 +1,14 @@
+int findDuplicate(vector<int> &arr) 
+{
+    // Write your code here
+	int ans = 0;
+    //xor all the elements of array
+    for(int i = 0; i<arr.size();i++){
+        ans = ans^arr[i];
+    }
+    //xor [1,n-1]
+    for(int i = 1; i<arr.size();i++){
+        ans = ans^arr[i];
+    }
+    return ans;
+}
